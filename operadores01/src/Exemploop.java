66c4e2399df9e82eff851c14563c09e463b19442
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-01-22 19:03:20 -0300

    Add files via upload

---
+++ operadores01/src/Exemploop.java
@@ -0,0 +1,8 @@
+
+public class Exemploop {
+
+	public static void main(String[] args) {
+		System.out.println("oi");
+		
+	}
+}
---
+++ operadores01/src/operadores01/Principal.java
@@ -0,0 +1,15 @@
+package operadores01;
+
+public class Principal {
+
+	public static void main (String[] args) {
+		byte x = 3;
+		short y = 4;
+		int z = 5;
+		long k = 6;
+		float u = 2;
+		double w = 3.0;
+		boolean t = true;
+	}
+}
+
